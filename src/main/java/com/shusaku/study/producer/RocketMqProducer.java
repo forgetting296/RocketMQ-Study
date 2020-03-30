44dd646443a81b14359fd5dc6dd1fb0eb570f3fc
liuzp <1131664758@qq.com>
2020-03-30 10:48:38 +0800

    创建rocket-mq　study 项目

---
+++ src/main/java/com/shusaku/study/producer/RocketMqProducer.java
@@ -0,0 +1,15 @@
+package com.shusaku.study.producer;
+
+/**
+ * @program: rocket-mq
+ * @description:
+ * @author: Shusaku
+ * @create: 2020-03-30 10:43
+ */
+public class RocketMqProducer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
